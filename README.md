5f2912013ef918aa9fc755aa937286881b51c713
Lukasz Wronka <lwronka@squaredesign.pl>
2017-04-27 15:06:21 +0200

    boilerplate

---
+++ README.md
@@ -0,0 +1,6 @@
+
+
+2. Run `npm install` to install all the dependencies
+3. Run `npm run dev` -developing mode
+4. Run `npm run prod` -production mode
+5. New browser window should open with a green text on pink background.
---
+++ src/app.js
@@ -0,0 +1,11 @@
+const css = require('./app.scss');
+const bootstrapCss = require('../node_modules/bootstrap/dist/css/bootstrap.min.css');
+const jquery = require('../node_modules/jquery/dist/jquery');
+const tether = require('../node_modules/tether/dist/js/tether');
+const bootstrapJs = require('../node_modules/bootstrap/dist/js/bootstrap');
+
+
+const test = require('./test');
+
+
+console.log('Hello from app.js!');
